fe885f6b4eac6885dc6cb18e53778ba3b0f527f4
ngockim109 <ngockim9047@gmail.com>
2024-07-01 15:21:17 +0700

    booking recepit react query

--- src/app/[locale]/(normalUser)/(auth)/(customer)/me/receipts/page.tsx
+++ src/app/[locale]/(normalUser)/(auth)/(customer)/me/receipts/page.tsx
@@ -1,29 +1,28 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
 
 import { getMyBookingReceipt } from "@/app/api/auth/booking-receipt";
 import BookingReceipt from "@/components/CustomerBooking/BookingReceipts";
-import type IBookingReceipt from "@/types/BookingReceipt";
 
 type Props = {};
 
 const Receipt: React.FC<Props> = () => {
-  const [bookings, setBookings] = useState<IBookingReceipt[]>([]);
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["bookingReceipts"],
+    queryFn: getMyBookingReceipt,
+  });
 
-  useEffect(() => {
-    const fetchBookings = async () => {
-      try {
-        const data = await getMyBookingReceipt();
-        console.log(data);
-        setBookings(data?.bookingList);
-      } catch (error) {
-        console.error("Failed to fetch booking receipts:", error);
-      }
-    };
+  const bookings = data?.bookingList ?? [];
 
-    fetchBookings();
-  }, []);
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error: {error?.message || "Failed to load data"}</div>;
+  }
 
   return (
     <div>
