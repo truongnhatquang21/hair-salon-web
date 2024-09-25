ec3ff9ba876176bfb88de38b48f5f5802b0adb8e
Truong Nhat Quang <quang.nhat.truong@skymavis.com>
2024-09-25 23:43:57 +0700

    fix: bug

--- src/apiCallers/AIchatBot/index.ts
+++ src/apiCallers/AIchatBot/index.ts
@@ -1,6 +1,31 @@
-'use server';
+import { responseMapping } from '@/lib/error';
 
-import { fetcher } from '..';
+const fetcher = async <T>(
+  url: string,
+  options?: RequestInit,
+  baseUrl?: string
+) => {
+  const res = await fetch((baseUrl || process.env.SERVER_URL) + url, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    cache: 'no-cache',
+
+    ...options,
+  });
+
+  const result = await res.json();
+  if (baseUrl) {
+    return result;
+  }
+  console.log('result', result);
+  console.log('Response OK', res);
+  const transformedResult = responseMapping<T>(result);
+  transformedResult.status = res.status;
+  transformedResult.ok = res.ok;
+
+  return transformedResult;
+};
 
 const URL = process.env.NEXT_PUBLIC_AI_ULR;
 type Res = {
